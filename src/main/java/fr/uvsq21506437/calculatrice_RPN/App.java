2397560de78adc30371f0c2332804a6de4431320
shadealao <alao.a.s@gmail.com>
2020-03-26 19:40:13 +0100

    projet init maven

---
+++ src/main/java/fr/uvsq21506437/calculatrice_RPN/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq21506437.calculatrice_RPN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
