4c633873c80c775c81624e9e2bcc4e8318152391
shadealao <alao.a.s@gmail.com>
2020-03-29 13:40:22 +0200

    ajout testCalculatrice()

--- src/test/java/fr/uvsq21506437/calculatriceRPN/CalculatriceRPNTest.java
+++ src/test/java/fr/uvsq21506437/calculatriceRPN/CalculatriceRPNTest.java
@@ -1,38 +1,32 @@
 package fr.uvsq21506437.calculatriceRPN;
 
-import junit.framework.Test;
+import fr.uvsq21506437.calculatriceRPN.exception.ActionNonSupporteeException;
+import fr.uvsq21506437.calculatriceRPN.exception.DivisionZeroException;
+import fr.uvsq21506437.calculatriceRPN.exception.ErreurDeSaisieException;
+import fr.uvsq21506437.calculatriceRPN.exception.EstPileVideException;
+import fr.uvsq21506437.calculatriceRPN.exception.PilePleineException;
+import fr.uvsq21506437.calculatriceRPN.exception.QteSymbolesException;
+import fr.uvsq21506437.calculatriceRPN.exception.SaisieVideException;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class CalculatriceRPNTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public CalculatriceRPNTest( String testName )
-    {
-        super( testName );
-    }
+public class CalculatriceRPNTest extends TestCase{
 
     /**
-     * @return the suite of tests being tested
+     * Test main : j'effectue un calcul sur une pile vide
      */
-    public static Test suite()
-    {
-        return new TestSuite( CalculatriceRPNTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testCalculatrice() {
+    	SaisieRPN saisie = new SaisieRPN();
+    	Throwable caught = null;
+        try {
+	        saisie.Calculer();
+		} catch (EstPileVideException | ActionNonSupporteeException | QteSymbolesException | PilePleineException | DivisionZeroException e) {
+			caught = e.fillInStackTrace();
+		}
+		
+        assertNotNull(caught);
+		assertSame(EstPileVideException.class, caught.getClass());
     }
 }
