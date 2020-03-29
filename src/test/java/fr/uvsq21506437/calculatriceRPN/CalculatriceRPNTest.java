ab5c2e3dd12c11b02578a394f4b7a44dbfcf44e3
shadealao <alao.a.s@gmail.com>
2020-03-29 13:41:14 +0200

    MAJ

--- src/test/java/fr/uvsq21506437/calculatriceRPN/CalculatriceRPNTest.java
+++ src/test/java/fr/uvsq21506437/calculatriceRPN/CalculatriceRPNTest.java
@@ -2,11 +2,9 @@ package fr.uvsq21506437.calculatriceRPN;
 
 import fr.uvsq21506437.calculatriceRPN.exception.ActionNonSupporteeException;
 import fr.uvsq21506437.calculatriceRPN.exception.DivisionZeroException;
-import fr.uvsq21506437.calculatriceRPN.exception.ErreurDeSaisieException;
 import fr.uvsq21506437.calculatriceRPN.exception.EstPileVideException;
 import fr.uvsq21506437.calculatriceRPN.exception.PilePleineException;
 import fr.uvsq21506437.calculatriceRPN.exception.QteSymbolesException;
-import fr.uvsq21506437.calculatriceRPN.exception.SaisieVideException;
 import junit.framework.TestCase;
 
 /**
